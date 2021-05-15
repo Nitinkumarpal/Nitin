b6a9697d36913a027b2ddd8091ff8607671d8a9a
Nitin <nitinjkpal@gmail.com>
2021-05-15 15:03:06 +0530

    nitin

--- Blogs/Blogs/settings.py
+++ Blogs/Blogs/settings.py
@@ -127,3 +127,5 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATIC_URL = '/static/'
 STATIC_URL = '/media/'
 # MEDIA_URL=[os.path.join(BASE_DIR,'media')]
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
